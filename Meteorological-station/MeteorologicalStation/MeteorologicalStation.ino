57f0fbd298bf619c44412ed5d5a9d4ac76ab24c8
Ondřej Kašpar <ondrej.kaspar@tmapy.cz>
2023-01-15 21:45:04 +0100

    format

--- Meteorological-station/MeteorologicalStation/MeteorologicalStation.ino
+++ Meteorological-station/MeteorologicalStation/MeteorologicalStation.ino
@@ -24,7 +24,7 @@ int mq2SensorAnalog = 33;
 
 // BME280
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define pressure_offset 3.3 // Used to adjust sensor reading to correct pressure for your location
+#define pressure_offset 3.3
 
 Adafruit_BME280 bme; // I2C
 unsigned long delayTime;
@@ -41,18 +41,17 @@ enum weather_type {
   rain        = -1,
   tstorms     = -2
 };
-weather_type current_wx; // Enable the current wx to be recorded
+weather_type current_wx;
 
-// An array structure to record pressure, temperaturre, humidity and weather state
 typedef struct {
-  float pressure;            // air pressure at the designated hour
-  float temperature;         // temperature at the designated hour
-  float humidity;            // humidity at the designated hour
-  weather_type wx_state_1hr; // weather state at 1-hour
-  weather_type wx_state_3hr; // weather state at 3-hour point
+  float pressure;            
+  float temperature;         
+  float humidity;            
+  weather_type wx_state_1hr; 
+  weather_type wx_state_3hr; 
 } wx_record_type;
 
-wx_record_type reading[24]; // An array covering 24-hours to enable P, T, % and Wx state to be recorded for every hour
+wx_record_type reading[24]; 
 
 bool look_3hr = true;
 bool look_1hr = false;
@@ -76,17 +75,17 @@ void setup() {
     }
   }
   configTime(0, 3600, "pool.ntp.org");
-  while (!update_time());  //Get the latest time
-  for (int i = 0; i <= 23; i++){ // At the start all array values are the same as a baseline 
-    reading[i].pressure     = read_pressure();       // A rounded to 1-decimal place version of pressure
-    reading[i].temperature  = bme.readTemperature(); // Although not used, but avialable
-    reading[i].humidity     = bme.readHumidity();    // Although not used, but avialable
-    reading[i].wx_state_1hr = unknown;               // To begin with  
-    reading[i].wx_state_3hr = unknown;               // To begin with 
+  while (!update_time()); 
+  for (int i = 0; i <= 23; i++){  
+    reading[i].pressure     = read_pressure();       
+    reading[i].temperature  = bme.readTemperature();
+    reading[i].humidity     = bme.readHumidity();    
+    reading[i].wx_state_1hr = unknown;               
+    reading[i].wx_state_3hr = unknown;               
   }
   last_reading_hour = reading_hour;
-  wx_average_1hr = 0; // Until we get a better idea
-  wx_average_3hr = 0; // Until we get a better idea                                                
+  wx_average_1hr = 0; 
+  wx_average_3hr = 0;                                                
   pinMode(rainDigital, INPUT);
 }
 
@@ -104,7 +103,7 @@ void loop() {
   int mq2AnalogVal = analogRead(mq2SensorAnalog);
 
   Serial.print("Gas Sensor: ");  
-  Serial.print(mq2AnalogVal);   /*Read value printed*/
+  Serial.print(mq2AnalogVal); 
   Serial.print("\t");
   Serial.print("\t");
   if (mq2AnalogVal > 400 && smoke_bool == false) {
@@ -121,7 +120,7 @@ void loop() {
 //  reading[23].pressure = (reading[23].pressure + read_pressure())/2;                 // Update rolling average
 //  float  trend = reading[23].pressure - reading[22].pressure;                        // Get short-term trend
 //  wx_text = get_forecast_text(read_pressure(), trend, look_1hr); // Convert to forecast text based on 1-hours
-  float  trend = reading[23].pressure - reading[20].pressure;                             // Get current trend over last 3-hours
+  float  trend = reading[23].pressure - reading[20].pressure;                             
   wx_text = get_forecast_text(reading[23].pressure, trend, look_3hr); 
   
   Serial.print("Počasí: ");
@@ -144,28 +143,28 @@ void loop() {
 }
 
 float read_pressure(){
-  int reading = (bme.readPressure()/100.0F+pressure_offset)*10; // Rounded result to 1-decimal place
+  int reading = (bme.readPressure()/100.0F+pressure_offset)*10;
   return (float)reading/10;
 }
 
 void update_time_and_data(){
   while (!update_time());
-  if (reading_hour != last_reading_hour) { // If the hour has advanced, then shift readings left and record new values at array element [23]
+  if (reading_hour != last_reading_hour) { 
     for (int i = 0; i < 23;i++){
       reading[i].pressure     = reading[i+1].pressure;
       reading[i].temperature  = reading[i+1].temperature;
       reading[i].wx_state_1hr = reading[i+1].wx_state_1hr;
       reading[i].wx_state_3hr = reading[i+1].wx_state_3hr;
     }
-    reading[23].pressure     = read_pressure(); // Update time=now with current value of pressure
+    reading[23].pressure     = read_pressure(); 
     reading[23].wx_state_1hr = current_wx;
     reading[23].wx_state_3hr = current_wx;
     last_reading_hour        = reading_hour;
     hr_cnt++;
-    wx_average_1hr = reading[22].wx_state_1hr + current_wx;           // Used to predict 1-hour forecast extra text
+    wx_average_1hr = reading[22].wx_state_1hr + current_wx;           
     wx_average_3hr = 0;
-    for (int i=23;i >= 21; i--){                                      // Used to predict 3-hour forecast extra text 
-      wx_average_3hr = wx_average_3hr + (int)reading[i].wx_state_3hr; // On average the last 3-hours of weather is used for the 'no change' forecast - e.g. more of the same?
+    for (int i=23;i >= 21; i--){                                    
+      wx_average_3hr = wx_average_3hr + (int)reading[i].wx_state_3hr;
     }
   }  
 }
@@ -176,18 +175,17 @@ bool update_time(){
     Serial.println("Failed to obtain time");
     return false;
   }
-  Serial.println(&timeinfo, "%A, %d %B %y %H:%M:%S"); // Displays: Saturday, 24 June 17 14:05:49
+  Serial.println(&timeinfo, "%A, %d %B %y %H:%M:%S"); 
   char strftime_buf[64];
   strftime(strftime_buf, sizeof(strftime_buf), "%R:%S   %a %d-%m-%y", &timeinfo);
-  time_str = strftime_buf;  // Now is this format HH:MM:SS Sat 05-07-17
+  time_str = strftime_buf;  
   reading_hour = time_str.substring(0,2).toInt();
   return true;
 }
 
-// Convert pressure and trend to a weather description either for 1 or 3 hours with the boolean true/false switch
 String get_forecast_text(float pressure_now, float trend, bool range) {
   String trend_str = get_trend_text(trend);
-  String wx_text = "No change, clearing"; //As a default forecast 
+  String wx_text = "No change, clearing"; 
   weather_extra_text = "";
   if (pressure_now >= 1022.68 ) {
     wx_text = "Good clear weather";
@@ -234,9 +232,8 @@ String get_forecast_text(float pressure_now, float trend, bool range) {
   return wx_text;
 }
 
-// Convert pressure trend to text
 String get_trend_text(float trend) {
-  String trend_str = "Steady"; // Default weather state
+  String trend_str = "Steady";
   if (trend > 3.5) { 
     trend_str = "Rising fast";  
   } else if (trend > 1.5 && trend <= 3.5) { 
@@ -256,7 +253,6 @@ String get_trend_text(float trend) {
   return trend_str;
 }
 
-// Convert 1-hr weather history to text
 void wx_history_1hr() {
   if      (wx_average_1hr >  0) weather_extra_text = ", expect sun";
   else if (wx_average_1hr == 0) weather_extra_text = ", mainly cloudy";
@@ -264,7 +260,6 @@ void wx_history_1hr() {
   else weather_extra_text = "";
 }
 
-// Convert 3-hr weather history to text
 void wx_history_3hr() {
   if      (wx_average_3hr >  0) weather_extra_text = ", expect sun";
   else if (wx_average_3hr == 0) weather_extra_text = ", mainly cloudy";
